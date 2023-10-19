8fcbfed2a36ecf9546b6e16487959b1f3c0d1b52
chris-pitre <c.tophanp3@gmail.com>
2023-10-19 15:34:45 -0400

    added waiting

--- Assets/Scripts/Components/PatrolComponent.cs
+++ Assets/Scripts/Components/PatrolComponent.cs
@@ -14,21 +14,35 @@ public class PatrolComponent : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float waypointThreshold;
+    [SerializeField] private float waitTime = 0f;
+    private float timer;
+    private bool isWaiting;
     private void Start(){
         transform.position = currentWaypoint.transform.position;
         movement = GetComponent<MovementComponent>();
         vision = GetComponent<VisionComponent>();
+        timer = waitTime;
     }
 
     private void Update(){
+        if(timer < waitTime){
+            timer += Time.deltaTime;
+            movement.DoMovementSilent(Vector2.zero, false);
+            return;
+        } 
+        if (isWaiting && timer >= waitTime){
+            currentWaypoint = currentWaypoint.GetNextWaypoint();
+            vision.LookTowards(currentWaypoint.transform.position);
+            isWaiting = false;
+        }
         if(vision.GetIsLookTowardsRunning()){
             movement.DoMovementSilent(Vector2.zero, false);
             return;
         }
         if(currentWaypoint.GetDistance(transform.position) <= waypointThreshold){
             movement.DoMovementSilent(Vector2.zero, false);
-            currentWaypoint = currentWaypoint.GetNextWaypoint();
-            vision.LookTowards(currentWaypoint.transform.position);
+            isWaiting = true;
+            timer = 0f;
         } else {
             movement.DoMovement(currentWaypoint.GetDirection(transform.position), false);
             vision.LookAt(currentWaypoint.transform.position);
