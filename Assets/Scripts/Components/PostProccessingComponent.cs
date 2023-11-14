aaeefba313437e3e14c979c24a10b7ea6be6305e
Henry Chin <hchin3@uncc.edu>
2023-11-14 17:23:38 -0500

    Implemented hurt visual effect

--- Assets/Scripts/Components/PostProccessingComponent.cs
+++ Assets/Scripts/Components/PostProccessingComponent.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
 
 public class PostProccessingComponent : MonoBehaviour
 {
+    [SerializeField] private Volume volume;
+    public Vignette vignette;
+    private FilmGrain filmGrain;
+
     // Start is called before the first frame update
     void Start()
     {
+        volume.profile.TryGet(out vignette);
+        volume.profile.TryGet(out filmGrain);
+        //vignette.color.value = Color.red;
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        //vignette.color.value = Color.Lerp(Color.red, Color.black, Time.time);
         
+    }
+
+    public IEnumerator Hurt()
+    {
+        for (float time = 0; time <= 1.05f; time += 0.05f)
+        {
+            vignette.color.value = Color.Lerp(Color.red, Color.black, time);
+            yield return null;
+        }
+        StopAllCoroutines();
     }
 }
